1914c15c48f30ba7470f3cf65daa8bf639ce03d6
Lyubosumaz <lyuboslavilkov@gmail.com>
2020-12-18 22:58:04 +0200

    complete Items CRUD

--- dist/resolvers/items.js
+++ dist/resolvers/items.js
@@ -11,6 +11,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ItemsResolver = void 0;
 const Items_1 = require("../entities/Items");
@@ -18,6 +27,40 @@ const type_graphql_1 = require("type-graphql");
 let ItemsResolver = class ItemsResolver {
     items({ em }) {
         return em.find(Items_1.Items, {});
+    }
+    item(id, { em }) {
+        return em.findOne(Items_1.Items, { id });
+    }
+    createItem(title, { em }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const post = em.create(Items_1.Items, { title });
+            yield em.persistAndFlush(post);
+            return post;
+        });
+    }
+    updateItem(id, title, { em }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const post = yield em.findOne(Items_1.Items, { id });
+            if (!post) {
+                return null;
+            }
+            if (typeof title !== 'undefined') {
+                post.title = title;
+                yield em.persistAndFlush(post);
+            }
+            return post;
+        });
+    }
+    deleteItem(id, { em }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                yield em.nativeDelete(Items_1.Items, { id });
+                return true;
+            }
+            catch (_a) {
+                return false;
+            }
+        });
     }
 };
 __decorate([
@@ -27,6 +70,39 @@ __decorate([
     __metadata("design:paramtypes", [Object]),
     __metadata("design:returntype", Promise)
 ], ItemsResolver.prototype, "items", null);
+__decorate([
+    type_graphql_1.Query(() => Items_1.Items, { nullable: true }),
+    __param(0, type_graphql_1.Arg('id', () => type_graphql_1.Int)),
+    __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number, Object]),
+    __metadata("design:returntype", Promise)
+], ItemsResolver.prototype, "item", null);
+__decorate([
+    type_graphql_1.Mutation(() => Items_1.Items),
+    __param(0, type_graphql_1.Arg('title', () => String)),
+    __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, Object]),
+    __metadata("design:returntype", Promise)
+], ItemsResolver.prototype, "createItem", null);
+__decorate([
+    type_graphql_1.Mutation(() => Items_1.Items, { nullable: true }),
+    __param(0, type_graphql_1.Arg('id', () => type_graphql_1.Int)),
+    __param(1, type_graphql_1.Arg('title', () => String, { nullable: true })),
+    __param(2, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number, String, Object]),
+    __metadata("design:returntype", Promise)
+], ItemsResolver.prototype, "updateItem", null);
+__decorate([
+    type_graphql_1.Mutation(() => Boolean),
+    __param(0, type_graphql_1.Arg('id', () => type_graphql_1.Int)),
+    __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number, Object]),
+    __metadata("design:returntype", Promise)
+], ItemsResolver.prototype, "deleteItem", null);
 ItemsResolver = __decorate([
     type_graphql_1.Resolver()
 ], ItemsResolver);
--- src/resolvers/items.ts
+++ src/resolvers/items.ts
@@ -1,11 +1,61 @@
 import { Items } from '../entities/Items';
 import { MyContest } from '../types';
-import { Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
 
 @Resolver()
 export class ItemsResolver {
     @Query(() => [Items])
     items(@Ctx() { em }: MyContest): Promise<Items[]> {
         return em.find(Items, {});
+    }
+
+    @Query(() => Items, { nullable: true })
+    item(
+        @Arg('id', () => Int) id: number, 
+        @Ctx() { em }: MyContest
+    ): Promise<Items | null> {
+        return em.findOne(Items, { id });
+    }
+
+    @Mutation(() => Items)
+    async createItem(
+        @Arg('title', () => String) title: string, 
+        @Ctx() { em }: MyContest
+    ): Promise<Items> {
+        const post = em.create(Items, { title });
+        await em.persistAndFlush(post);
+        return post;
+    }
+
+    @Mutation(() => Items, { nullable: true })
+    async updateItem(
+        @Arg('id', () => Int) id: number, 
+        @Arg('title', () => String, { nullable: true }) title: string, 
+        @Ctx() { em }: MyContest
+    ): Promise<Items | null> {
+        const post = await em.findOne(Items, { id });
+        if (!post) {
+            return null;
+        }
+
+        if (typeof title !== 'undefined') {
+            post.title = title;
+            await em.persistAndFlush(post);
+        }
+
+        return post;
+    }
+
+    @Mutation(() => Boolean)
+    async deleteItem(
+        @Arg('id', () => Int) id: number, 
+        @Ctx() { em }: MyContest
+    ): Promise<boolean> {
+        try {
+            await em.nativeDelete(Items, { id })
+            return true;
+        } catch {
+            return false;
+        }
     }
 }
