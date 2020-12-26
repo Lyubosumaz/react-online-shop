9cd0137ed6246d7d526260e0ab4258d1122fa5d4
Lyubosumaz <lyuboslavilkov@gmail.com>
2020-12-26 20:44:33 +0200

    add page forgotten password

--- web/src/pages/forgotten-password.tsx
+++ web/src/pages/forgotten-password.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import { withUrqlClient } from 'next-urql';
+import React, { useState } from 'react';
+import { Field, withTypes } from 'react-final-form';
+import { createUrqlClient } from '../utils/createUrqlClient';
 
-interface forgottenPasswordProps {}
+interface ErrType {
+    [key: string]: string;
+}
 
-const forgottenPassword: React.FC<{}> = ({}) => {
-    return <div>forgotten password</div>;
+type MyValues = {
+    email: string;
 };
 
-export default forgottenPassword;
+const { Form } = withTypes<MyValues>();
+const required = (value: any) => (value ? undefined : 'Required');
+
+const forgottenPassword: React.FC<{}> = ({}) => {
+    const [errors, setErrors] = useState({} as ErrType);
+
+    return (
+        <>
+            <Form
+                onSubmit={async (values: MyValues) => {}}
+                render={({ handleSubmit }) => (
+                    <form onSubmit={handleSubmit}>
+                        <Field name="email" validate={required}>
+                            {({ input, meta }) => (
+                                <div>
+                                    <label>Email</label>
+                                    <input {...input} type="text" placeholder="email" />
+                                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                                    {errors['email'] ? <div>{errors['email']}</div> : <div>123</div>}
+                                </div>
+                                // {meta.validating && <div className={stylesSpinner.div}></div>}
+                            )}
+                        </Field>
+
+                        <div className="buttons">
+                            <button type="submit">Submit</button>
+                        </div>
+                    </form>
+                )}
+            />
+        </>
+    );
+};
+
+export default withUrqlClient(createUrqlClient)(forgottenPassword);
