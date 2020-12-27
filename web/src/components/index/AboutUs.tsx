319c6aeb78da667cb0eee378b24de12ff2c09c0d
Lyubosumaz <lyuboslavilkov@gmail.com>
2020-12-28 01:56:29 +0200

    update styles

--- web/src/components/index/AboutUs.tsx
+++ web/src/components/index/AboutUs.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import site from '../../styles/scss/Site.module.scss';
 import styles from '../../styles/scss/AboutUs.module.scss';
 import Wrapper from '../site/Wrapper';
+import NextLink from 'next/link';
 
 const AboutUs: React.FC<{}> = ({}) => {
     return (
         <>
-            <Wrapper>
-                <div className={[styles.column].join(' ')}>sda</div>
-                <div className={[styles.column].join(' ')}>dsad</div>
-            </Wrapper>
+            <div className={styles['about-us']}>
+                <Wrapper>
+                    <div className={[site.column].join(' ')}>
+                        <div className={styles['image-wrapper']}>
+                            <div className="actual-image"></div>
+                        </div>
+                    </div>
+                    <div className={[site.column, styles.description].join(' ')}>
+                        <h5>About us</h5>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomized</p>
+                        <div className={`button-wrapper`}>
+                            <NextLink href="/">
+                                <a>Read More</a>
+                            </NextLink>
+                        </div>
+                    </div>
+                </Wrapper>
+            </div>
         </>
     );
 };
--- web/src/components/index/ContactUs.tsx
+++ web/src/components/index/ContactUs.tsx
@@ -1,6 +1,8 @@
-import Link from 'next/link';
+import NextLink from 'next/link';
 import React from 'react';
+import site from '../../styles/scss/Site.module.scss';
 import styles from '../../styles/ContactUs.module.scss';
+
 import Wrapper from '../site/Wrapper';
 
 const ContactUs: React.FC<{}> = ({}) => {
@@ -8,18 +10,18 @@ const ContactUs: React.FC<{}> = ({}) => {
         <>
             <div className={styles['contact-us']}>
                 <Wrapper>
-                    <div className={[styles.column, styles.description].join(' ')}>
+                    <div className={[site.column, styles.description].join(' ')}>
                         <h2>
                             <span>Best</span>
                             <span>Design</span>
                             <span>of Furniture</span>
                         </h2>
                         <p>Is is a long established fact that a reader will be distracted by the readable content of</p>
-                        <Link href="/">
+                        <NextLink href="/">
                             <a>Contact us</a>
-                        </Link>
+                        </NextLink>
                     </div>
-                    <div className={[styles.column, styles.image].join(' ')}>
+                    <div className={[site.column, styles.image].join(' ')}>
                         <span>Image</span>
                     </div>
                 </Wrapper>
