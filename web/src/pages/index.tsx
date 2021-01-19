b9e693cbc28f90127bf602bac22151666b430fd9
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-01-19 23:53:02 +0200

    rewoking components folder

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { withUrqlClient } from 'next-urql';
 import { useState } from 'react';
 import AboutUs from '../components/index/AboutUs';
-import BestDesign from '../components/index/BestDesign';
+import BestDesign from '../components/index/BestDesign/BestDesign';
 import OurWork from '../components/index/OurWork';
 import { useItemsQuery } from '../generated/graphql';
 import styles from '../styles/scss/4-pages/Home.module.scss';
