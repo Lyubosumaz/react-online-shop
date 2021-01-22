e968823bc960c16cdde73701c28e8db367571bab
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-01-23 00:08:04 +0200

    updateing AboutUs component styles

--- web/src/components/index/AboutUs/index.tsx
+++ web/src/components/index/AboutUs/index.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import Wrapper from '../../../layouts/MainWrapper';
-import site from '../../../styles/scss/2-basics/Site.module.scss';
-import styles from '../../../styles/scss/3-components/AboutUs.module.scss';
 import SecondaryButton from '../../buttons/SecondaryButton';
+import styles from './AboutUs.module.scss';
 
 const AboutUs: React.FC<{}> = ({}) => {
     return (
         <>
             <div className={styles[`about-us`]}>
                 <Wrapper>
-                    <div className={[site.column].join(' ')}>
+                    <div className={styles.column}>
                         <div className={styles[`image-wrapper`]}>
-                            <div className={site[`image`]}>
+                            <div className={styles.image}>
                                 <span>Image</span>
                             </div>
                         </div>
                     </div>
 
-                    <div className={[site.column, styles.description].join(' ')}>
+                    <div className={[styles.column, styles.description].join(' ')}>
                         <h5>About us</h5>
 
                         <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomized</p>
