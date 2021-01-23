8e0bc3e941d167e7746bc4ffecd3a7ab0d3fe367
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-01-23 04:29:28 +0200

    adding test page to test new form component

--- web/src/layouts/FormWrapper/index.tsx
+++ web/src/layouts/FormWrapper/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { withTypes } from 'react-final-form';
+import Wrapper from '../MainWrapper';
 
-const FormWrapper: React.FC<{}> = ({}) => {
+type MyValues = {
+    username: string;
+    email: string;
+    password: string;
+};
+
+interface FormWrapperProps {
+    children: React.ReactNode;
+    fieldType?: any;
+    funcOnSubmit?: any;
+}
+
+const FormWrapper: React.FC<FormWrapperProps> = ({ children, fieldType, funcOnSubmit }) => {
+    const { Form } = withTypes<MyValues>();
+
     return (
         <>
-            <h1>FormWrapper</h1>
-            <div>FormWrapper</div>
+            <Form
+                onSubmit={
+                    // this should be funcOnSubmit
+                    async () => {}
+                }
+                render={({ handleSubmit }) => (
+                    <form onSubmit={handleSubmit}>
+                        <Wrapper>{children}</Wrapper>
+                    </form>
+                )}
+            />
         </>
     );
 };
---
+++ web/src/pages/test/index.tsx
@@ -0,0 +1,71 @@
+import { withUrqlClient } from 'next-urql';
+import React, { useState } from 'react';
+import { Field } from 'react-final-form';
+import MainButton from '../../components/buttons/MainButton';
+import FormWrapper from '../../layouts/FormWrapper';
+import styles from '../../styles/scss/Forms.module.scss';
+// import stylesSpinner from '../../styles/scss/Spinner.module.scss';
+import { createUrqlClient } from '../../utils/createUrqlClient';
+
+interface ErrType {
+    [key: string]: string;
+}
+
+type MyValues = {
+    username: string;
+    email: string;
+    password: string;
+};
+
+const Register: React.FC<{}> = ({}) => {
+    const [errors, setErrors] = useState({} as ErrType);
+    const required = (value: any) => (value ? undefined : 'Required');
+
+    return (
+        <>
+            <FormWrapper fieldType={'hmm'}>
+                <Field name="username" validate={required}>
+                    {({ input, meta }) => (
+                        <div>
+                            <label>Username:</label>
+                            <input {...input} type="text" placeholder="John Doe" />
+                            {meta.error && meta.touched && <span>{meta.error}</span>}
+                            {errors['username'] ? <div>{errors['username']}</div> : <div>123</div>}
+                            {/* {meta.validating && <div className={stylesSpinner.div}></div>} */}
+                        </div>
+                    )}
+                </Field>
+
+                <Field name="email" validate={required}>
+                    {({ input, meta }) => (
+                        <div>
+                            <label>Email:</label>
+                            <input {...input} type="text" placeholder="john@doe.com" />
+                            {meta.error && meta.touched && <span>{meta.error}</span>}
+                            {errors['email'] ? <div>{errors['email']}</div> : <div>123</div>}
+                            {/* {meta.validating && <div className={stylesSpinner.div}></div>} */}
+                        </div>
+                    )}
+                </Field>
+
+                <Field name="password" validate={required}>
+                    {({ input, meta }) => (
+                        <div>
+                            <label>Password:</label>
+                            <input {...input} type="password" placeholder="********" />
+                            {meta.error && meta.touched && <span>{meta.error}</span>}
+                            {errors['password'] ? <div>{errors['password']}</div> : <div>123</div>}
+                            {/* {meta.validating && <div className={stylesSpinner.div}></div>} */}
+                        </div>
+                    )}
+                </Field>
+
+                <div className={styles[`button-wrapper`]}>
+                    <MainButton text={'Register'} type={'submit'} />
+                </div>
+            </FormWrapper>
+        </>
+    );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: false })(Register);
