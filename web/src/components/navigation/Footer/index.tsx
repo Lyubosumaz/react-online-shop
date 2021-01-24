76a52b55e668156e689758029f0882d5f40f997c
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-01-24 23:56:31 +0200

    update MainWrapper and Footer component styles

--- web/src/components/navigation/Footer/index.tsx
+++ web/src/components/navigation/Footer/index.tsx
@@ -1,5 +1,6 @@
 import { withUrqlClient } from 'next-urql';
 import React from 'react';
+import Wrapper from '../../../layouts/MainWrapper';
 import { createUrqlClient } from '../../../utils/createUrqlClient';
 import styles from './Footer.module.scss';
 
@@ -7,52 +8,54 @@ const Footer: React.FC<{}> = ({}) => {
     return (
         <>
             <footer className={styles[`site-footer`]}>
-                <ul className={`footer-shop`}>
-                    <li>Lorem lpusm hosting web</li>
-                    <li>Call: +7586656566</li>
-                    <li>demo@mail.com</li>
-                </ul>
+                <Wrapper>
+                    <ul className={styles[`footer-shop`]}>
+                        <li>Lorem lpusm hosting web</li>
+                        <li>Call: +7586656566</li>
+                        <li>demo@mail.com</li>
+                    </ul>
 
-                <ul>
-                    <li>
-                        <header>
-                            <h5>Useful Link</h5>
-                        </header>
+                    <ul className={styles[`footer-services`]}>
+                        <li>
+                            <header>
+                                <h5>Useful Link</h5>
+                            </header>
 
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Our Designe</li>
-                            <li>Contact Us</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <header>
-                            <h5>Repair</h5>
-                        </header>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Our Designe</li>
+                                <li>Contact Us</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <header>
+                                <h5>Repair</h5>
+                            </header>
 
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit 30Levcamsamoajajahavlovemorethenthatajaj.</p>
-                    </li>
-                    <li>
-                        <header>
-                            <h5>Social Media</h5>
-                        </header>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit 30Levcamsamoajajahavlovemorethenthatajaj.</p>
+                        </li>
+                        <li>
+                            <header>
+                                <h5>Social Media</h5>
+                            </header>
 
-                        <ul>
-                            <li>Facebook</li>
-                            <li>Tweeter</li>
-                            <li>Google+</li>
-                            <li>Linkedin</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <header>
-                            <h5>Our Repair Center</h5>
-                        </header>
+                            <ul>
+                                <li>Facebook</li>
+                                <li>Tweeter</li>
+                                <li>Google+</li>
+                                <li>Linkedin</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <header>
+                                <h5>Our Repair Center</h5>
+                            </header>
 
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit 30Levcamsamoajajahavlovemorethenthatajaj.</p>
-                    </li>
-                </ul>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit 30Levcamsamoajajahavlovemorethenthatajaj.</p>
+                        </li>
+                    </ul>
+                </Wrapper>
 
                 <div className={styles[`footer-rights`]}>
                     <p>2020-{new Date().getFullYear()} All Rights Restricted. Design by Free html Templates</p>
