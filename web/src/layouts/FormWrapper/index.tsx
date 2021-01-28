aaa3ba490c65b2b58d7b864603a6535543ae03f7
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-01-28 20:58:18 +0200

    pushing some react-final-form

--- web/src/layouts/FormWrapper/index.tsx
+++ web/src/layouts/FormWrapper/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import { withTypes } from 'react-final-form';
+import { useRegisterMutation } from '../../generated/graphql';
+import { toErrorMap } from '../../utils/toErrorMap';
 import Wrapper from '../MainWrapper';
 
 interface FormWrapperProps {
@@ -12,8 +15,39 @@ type MyValues = {
     [key: string]: string;
 };
 
+type registerValues = {
+    username: string;
+    email: string;
+    password: string;
+};
+
+interface ErrType {
+    [key: string]: string;
+}
 const FormWrapper: React.FC<FormWrapperProps> = ({ children, fieldType, funcOnSubmit }) => {
     const { Form } = withTypes<MyValues>();
+    const router = useRouter();
+    const [errors, setErrors] = useState({} as ErrType);
+
+    switch ('register') {
+        case 'register':
+            const [, register] = useRegisterMutation();
+
+            async (values: registerValues) => {
+                if (values.username && values.password) {
+                    const response = await register({ options: values });
+                    if (response.data?.register.errors) {
+                        setErrors(toErrorMap(response.data.register.errors));
+                    } else if (response.data?.register.user) {
+                        setErrors({});
+                        router.push('/');
+                    }
+                }
+            };
+            break;
+        default:
+            break;
+    }
 
     return (
         <>
--- web/src/pages/test/index.tsx
+++ web/src/pages/test/index.tsx
@@ -1,70 +1,19 @@
 import { withUrqlClient } from 'next-urql';
-import React, { useState } from 'react';
-import { Field } from 'react-final-form';
+import React from 'react';
 import MainButton from '../../components/buttons/MainButton';
 import FieldFactory from '../../components/form/FieldFactory';
 import FormWrapper from '../../layouts/FormWrapper';
 import styles from '../../styles/scss/Forms.module.scss';
-// import stylesSpinner from '../../styles/scss/Spinner.module.scss';
 import { createUrqlClient } from '../../utils/createUrqlClient';
 
-interface ErrType {
-    [key: string]: string;
-}
-
 const Register: React.FC<{}> = ({}) => {
-    const [errors, setErrors] = useState({} as ErrType);
-    const required = (value: any) => (value ? undefined : 'Required');
-
     return (
         <>
-            <FormWrapper fieldType={'hmm'}>
+            <FormWrapper>
                 <FieldFactory fieldName={`username8`} fieldLabel={`Username8`} fieldType={`text`} fieldPlaceholder={`88`} />
 
-                <Field name="username" validate={required}>
-                    {({ input, meta }) => (
-                        <div>
-                            <label>Username:</label>
-                            <input {...input} type="text" placeholder="John Doe" />
-                            {meta.error && meta.touched && <span>{meta.error}</span>}
-                            {errors['username'] ? <div>{errors['username']}</div> : <div>123</div>}
-                            {/* {meta.validating && <div className={stylesSpinner.div}></div>} */}
-                        </div>
-                    )}
-                </Field>
-                <Field name="username2" validate={required}>
-                    {({ input, meta }) => (
-                        <div>
-                            <label>Username2:</label>
-                            <input {...input} type="text" placeholder="John Doe" />
-                            {meta.error && meta.touched && <span>{meta.error}</span>}
-                            {errors['username2'] ? <div>{errors['username2']}</div> : <div>123</div>}
-                            {/* {meta.validating && <div className={stylesSpinner.div}></div>} */}
-                        </div>
-                    )}
-                </Field>
-                <Field name="email" validate={required}>
-                    {({ input, meta }) => (
-                        <div>
-                            <label>Email:</label>
-                            <input {...input} type="text" placeholder="john@doe.com" />
-                            {meta.error && meta.touched && <span>{meta.error}</span>}
-                            {errors['email'] ? <div>{errors['email']}</div> : <div>123</div>}
-                            {/* {meta.validating && <div className={stylesSpinner.div}></div>} */}
-                        </div>
-                    )}
-                </Field>
-                <Field name="password" validate={required}>
-                    {({ input, meta }) => (
-                        <div>
-                            <label>Password:</label>
-                            <input {...input} type="password" placeholder="********" />
-                            {meta.error && meta.touched && <span>{meta.error}</span>}
-                            {errors['password'] ? <div>{errors['password']}</div> : <div>123</div>}
-                            {/* {meta.validating && <div className={stylesSpinner.div}></div>} */}
-                        </div>
-                    )}
-                </Field>
+                <FieldFactory fieldName={`password8`} fieldLabel={`Password8`} fieldType={`password`} fieldPlaceholder={`88--`} />
+
                 <div className={styles[`button-wrapper`]}>
                     <MainButton text={'Register'} type={'submit'} />
                 </div>
