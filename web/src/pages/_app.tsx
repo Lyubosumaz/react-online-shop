9733423d0bf48db78b022a1e7d64f058682d66d2
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-02-03 23:37:22 +0200

    quick save pages

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import { CSSReset, ThemeProvider } from "@chakra-ui/core";
-import theme from "../theme";
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
+import theme from '../theme';
 
 function MyApp({ Component, pageProps }: any) {
-  return (
-    <ThemeProvider theme={theme}>
-      <CSSReset />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <CSSReset />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
 export default MyApp;
--- web/src/pages/post/[id].tsx
+++ web/src/pages/post/[id].tsx
@@ -1,45 +1,40 @@
-import React from "react";
-import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../../utils/createUrqlClient";
-import { Layout } from "../../components/Layout";
-import { Heading, Box } from "@chakra-ui/core";
-import { useGetPostFromUrl } from "../../utils/useGetPostFromUrl";
-import { EditDeletePostButtons } from "../../components/EditDeletePostButtons";
-import { withApollo } from "../../utils/withApollo";
+import { Box, Heading } from '@chakra-ui/core';
+import React from 'react';
+import { EditDeletePostButtons } from '../../components/EditDeletePostButtons';
+import { Layout } from '../../components/Layout';
+import { useGetPostFromUrl } from '../../utils/useGetPostFromUrl';
+import { withApollo } from '../../utils/withApollo';
 
 const Post = ({}) => {
-  const { data, error, loading } = useGetPostFromUrl();
+    const { data, error, loading } = useGetPostFromUrl();
 
-  if (loading) {
+    if (loading) {
+        return (
+            <Layout>
+                <div>loading...</div>
+            </Layout>
+        );
+    }
+
+    if (error) {
+        return <div>{error.message}</div>;
+    }
+
+    if (!data?.post) {
+        return (
+            <Layout>
+                <Box>could not find post</Box>
+            </Layout>
+        );
+    }
+
     return (
-      <Layout>
-        <div>loading...</div>
-      </Layout>
+        <Layout>
+            <Heading mb={4}>{data.post.title}</Heading>
+            <Box mb={4}>{data.post.text}</Box>
+            <EditDeletePostButtons id={data.post.id} creatorId={data.post.creator.id} />
+        </Layout>
     );
-  }
-
-  if (error) {
-    return <div>{error.message}</div>;
-  }
-
-  if (!data?.post) {
-    return (
-      <Layout>
-        <Box>could not find post</Box>
-      </Layout>
-    );
-  }
-
-  return (
-    <Layout>
-      <Heading mb={4}>{data.post.title}</Heading>
-      <Box mb={4}>{data.post.text}</Box>
-      <EditDeletePostButtons
-        id={data.post.id}
-        creatorId={data.post.creator.id}
-      />
-    </Layout>
-  );
 };
 
 export default withApollo({ ssr: true })(Post);
