5c0935d49d0e67c5314e13cf2c852aa777d5c882
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-02-04 16:44:24 +0200

    save change

--- my-save/server/src/types.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-import { Redis } from 'ioredis';
-
-export type MyContext = {
-    // @ts-ignore
-    req: Request & { session: Express.Session };
-    res: Response;
-    redis: Redis;
-};
--- my-save/web/src/layouts/MainWrapper/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './MainWrapper.module.scss';
-
-const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return <div className={styles.wrapper}>{children}</div>;
-};
-
-export default Wrapper;
