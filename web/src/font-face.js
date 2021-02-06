98ad04ef65e00ffc0be236f899e095c31253de45
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-02-06 23:28:24 +0200

    updating chakra-ui theme

---
+++ web/src/font-face.js
@@ -0,0 +1,26 @@
+export default `
+         @font-face {
+          font-family: 'Montserrat';
+          font-style: normal;
+          font-weight: 400;
+          font-display: swap;
+          src: local('Montserrat Regular'), local('Montserrat-Regular'), url(https://fonts.gstatic.com/s/montserrat/v14/JTUSjIg1_i6t8kCHKm459WlhyyTh89Y.woff2) format('woff2');
+          unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
+        @font-face {
+          font-family: 'Roboto';
+          font-style: normal;
+          font-weight: 400;
+          font-display: swap;
+          src: local('Roboto'), local('Roboto-Regular'), url(https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2) format('woff2');
+          unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
+        @font-face {
+          font-family: 'Poppins';
+          font-style: normal;
+          font-weight: 400;
+          font-display: swap;
+          src: local('Poppins'), local('Poppins-Regular'), url(https://fonts.googleapis.com/css2?family=Poppins&display=swap) format('woff2');
+          unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
+  `;
--- web/src/theme.tsx
+++ web/src/theme.tsx
@@ -1,17 +1,18 @@
 import { theme as chakraTheme } from '@chakra-ui/core';
 
-const fonts = { ...chakraTheme.fonts, mono: `'Menlo', monospace` };
-
-const breakpoints = ['40em', '52em', '64em'];
-
 const theme = {
     ...chakraTheme,
     colors: {
         ...chakraTheme.colors,
         black: '#16161D',
     },
-    fonts,
-    breakpoints,
+    fonts: {
+        ...chakraTheme.fonts,
+        // body: 'Montserrat, sans-serif',
+        heading: 'Montserrat, Georgia, sans-serif',
+        mono: 'Menlo, monospace',
+    },
+    breakpoints: ['40em', '52em', '64em'],
     icons: {
         ...chakraTheme.icons,
         logo: {
