4cacf99b32c53520b8b740ab142dd10635747ca3
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-02-09 23:48:35 +0200

    update Header

--- web/src/components/navigation/Header/index.tsx
+++ web/src/components/navigation/Header/index.tsx
@@ -1,9 +1,20 @@
 import { useApolloClient } from '@apollo/client';
-import { Box, Button, Flex, Heading, Link } from '@chakra-ui/react';
+import { Box, Button, Flex, Icon, IconButton, Link, Text } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import React from 'react';
+import { FaShoppingCart, FaStore } from 'react-icons/fa';
 import { useLogoutMutation, useMeQuery } from '../../../generated/graphql';
+import MainWrapper from '../../../layouts/MainWrapper';
 import { isServer } from '../../../utils/isServer';
+
+const FooterNavItem: React.FC<{ href: string }> = ({ children, href, ...rest }) => (
+    <NextLink href={href}>
+        {/* <Button as={Link} mr={4} textTransform="uppercase" backgroundColor="#7c2c0c" _hover={{ backgroundColor: '#efe4d1', color: '#7c2c0c' }}> */}
+        <Button as={Link} mr={4} textTransform="uppercase" {...rest}>
+            {children}
+        </Button>
+    </NextLink>
+);
 
 const Header: React.FC<{}> = ({}) => {
     const [logout, { loading: logoutFetching }] = useLogoutMutation();
@@ -13,35 +24,43 @@ const Header: React.FC<{}> = ({}) => {
     });
 
     return (
-        <header style={{ backgroundColor: '#7c2c0c', color: '#fcfcfc' }}>
-            <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4}>
-                <Flex flex={1} m="auto" align="center" maxW={800} backgroundColor="#7c2c0c">
+        <header style={{ padding: '0.5rem 0', position: 'static', top: 0, backgroundColor: '#7c2c0c', color: '#efe4d1', borderTop: '1rem solid #efe4d1', borderBottom: '1rem solid #efe4d1', zIndex: 1 }}>
+            <MainWrapper>
+                <Flex flex={1} align="center" backgroundColor="#7c2c0c">
                     <NextLink href="/">
-                        <Link>
-                            <Heading>LiReddit</Heading>
+                        <Link p={2} display="flex" alignItems="center" border="0.175rem solid transparent" borderRadius={8} _hover={{ borderColor: '#fcfcfc', color: '#fcfcfc' }}>
+                            <Icon as={FaStore} w={8} h={8} />
+                            <Text fontSize="2.5rem" fontWeight="700" lineHeight={1}>
+                                ROS
+                            </Text>
                         </Link>
                     </NextLink>
                     <Box ml={'auto'}>
                         {!data?.me ? (
                             <>
-                                <NextLink href="/login">
-                                    <Button as={Link} mr={4}>
-                                        <Link mr={2}>login</Link>
-                                    </Button>
-                                </NextLink>
-                                <NextLink href="/register">
-                                    <Link>register</Link>
-                                </NextLink>
+                                <FooterNavItem href="/login">Login</FooterNavItem>
+                                <FooterNavItem href="/login">Register</FooterNavItem>
                             </>
                         ) : (
                             <Flex align="center">
-                                <NextLink href="/create-item">
-                                    <Button as={Link} mr={4} textTransform="uppercase" backgroundColor="#7c2c0c" color="#efe4d1" _hover={{ backgroundColor: '#efe4d1', color: '#7c2c0c' }}>
-                                        create item
-                                    </Button>
-                                </NextLink>
-                                <Box mr={2}>Welcome {data.me.username}</Box>
+                                <FooterNavItem href="/create-item">Create Item</FooterNavItem>
+                                <FooterNavItem href="/forgotten-password">Recover Password</FooterNavItem>
+                                <Flex mr={4} alignItems="center">
+                                    <Text mr={2}>Welcome:</Text>
+                                    <NextLink href="/">
+                                        <Button as={Link} p="0 0.75em" textTransform="uppercase" backgroundColor="#7c2c0c" _hover={{ backgroundColor: '#efe4d1', color: '#7c2c0c' }}>
+                                            {data.me.username}
+                                        </Button>
+                                    </NextLink>
+                                </Flex>
+                                <IconButton mr={4} aria-label="Search database" icon={<FaShoppingCart />} backgroundColor="#7c2c0c" _hover={{ backgroundColor: '#efe4d1', color: '#7c2c0c' }} />
                                 <Button
+                                    height="2.5rem"
+                                    padding="0 1rem"
+                                    textTransform="uppercase"
+                                    backgroundColor="#7c2c0c"
+                                    color="#efe4d1"
+                                    _hover={{ backgroundColor: '#efe4d1', color: '#7c2c0c', textDecoration: 'underline' }}
                                     onClick={async () => {
                                         await logout();
                                         await apolloClient.resetStore();
@@ -49,13 +68,13 @@ const Header: React.FC<{}> = ({}) => {
                                     isLoading={logoutFetching}
                                     variant="link"
                                 >
-                                    logout
+                                    Logout
                                 </Button>
                             </Flex>
                         )}
                     </Box>
                 </Flex>
-            </Flex>
+            </MainWrapper>
         </header>
     );
 };
