9cc1f77da2b253c596f74fdff9683a511ac7f175
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-02-09 17:24:55 +0200

    update UtilityLinks

--- web/src/components/navigation/Footer/UtilityLinks/index.tsx
+++ web/src/components/navigation/Footer/UtilityLinks/index.tsx
@@ -1,4 +1,4 @@
-import { Heading, IconButton, Link, List, ListItem, Text } from '@chakra-ui/react';
+import { CSSWithMultiValues, Heading, IconButton, Link, List, ListItem, Text } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import React from 'react';
 import { FaFacebookF, FaGoogle, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
@@ -9,6 +9,30 @@ const SectionHeading: React.FC<{}> = ({ children, ...props }) => (
     </Heading>
 );
 
+const SiteLinksItem: React.FC<{ href: string }> = ({ children, href }) => (
+    <ListItem>
+        <NextLink href={href}>
+            <Link>{children}</Link>
+        </NextLink>
+    </ListItem>
+);
+interface MediaLinksItemProps {
+    href: string;
+    icon: any;
+    label: string;
+    backgroundColor?: string;
+    hover?: CSSWithMultiValues;
+    isLast?: boolean;
+}
+
+const MediaLinksItem: React.FC<MediaLinksItemProps> = ({ href, icon, label, backgroundColor, hover, isLast }) => (
+    <ListItem mr={isLast ? 0 : 1}>
+        <Link href={href}>
+            <IconButton size="lg" fontSize="1.5rem" icon={icon} aria-label={label} borderRadius="full" backgroundColor={!backgroundColor ? '#171717' : backgroundColor} _hover={!hover ? { backgroundColor: '#555' } : hover} />
+        </Link>
+    </ListItem>
+);
+
 const UtilityLinks: React.FC<{}> = ({}) => {
     return (
         <List pb="4rem" display="flex" justifyContent="space-between">
@@ -17,71 +41,31 @@ const UtilityLinks: React.FC<{}> = ({}) => {
 
                 <List spacing={2} opacity="0.5">
                     {/* TODO links paths need adding */}
-                    <ListItem>
-                        <NextLink href="/">
-                            <Link>Home</Link>
-                        </NextLink>
-                    </ListItem>
-                    <ListItem>
-                        <NextLink href="/">
-                            <Link>About</Link>
-                        </NextLink>
-                    </ListItem>
-                    <ListItem>
-                        <NextLink href="/">
-                            <Link>Our Design</Link>
-                        </NextLink>
-                    </ListItem>
-                    <ListItem>
-                        <NextLink href="/">
-                            <Link>Contact Us</Link>
-                        </NextLink>
-                    </ListItem>
+                    <SiteLinksItem href="/">Home</SiteLinksItem>
+                    <SiteLinksItem href="/">About</SiteLinksItem>
+                    <SiteLinksItem href="/">Our Design</SiteLinksItem>
+                    <SiteLinksItem href="/">Contact Us</SiteLinksItem>
                 </List>
             </ListItem>
 
             <ListItem maxWidth="8rem">
-                <Heading as="h4" pb={8} fontSize="1.5rem" textTransform="uppercase" whiteSpace="nowrap">
-                    Repair
-                </Heading>
-
+                <SectionHeading>Repair</SectionHeading>
                 <Text opacity="0.5">Lorem ipsum dolor sit, amet conse ctet ur adipi sicing elit 30Levcamsamoajajahavlove.</Text>
             </ListItem>
 
             <ListItem maxWidth="13rem">
-                <Heading as="h4" pb={8} fontSize="1.5rem" textTransform="uppercase" whiteSpace="nowrap">
-                    Social Media
-                </Heading>
+                <SectionHeading>Social Media</SectionHeading>
 
                 <List display="flex">
-                    <ListItem mr={1}>
-                        <Link href="https://www.facebook.com/">
-                            <IconButton size="lg" fontSize="1.5rem" icon={<FaFacebookF />} borderRadius="full" aria-label="Facebook" backgroundColor="#7c2c0c" _hover={{ backgroundColor: '#efe4d1', color: '#3b5998' }} />
-                        </Link>
-                    </ListItem>
-                    <ListItem mr={1}>
-                        <Link href="https://twitter.com/">
-                            <IconButton size="lg" fontSize="1.5rem" icon={<FaTwitter />} borderRadius="full" aria-label="Twitter" backgroundColor="#171717" _hover={{ backgroundColor: '#555' }} />
-                        </Link>
-                    </ListItem>
-                    <ListItem mr={1}>
-                        <Link href="https://www.google.com/">
-                            <IconButton size="lg" fontSize="1.5rem" icon={<FaGoogle />} borderRadius="full" aria-label="Google" backgroundColor="#171717" _hover={{ backgroundColor: '#555' }} />
-                        </Link>
-                    </ListItem>
-                    <ListItem>
-                        <Link href="https://www.linkedin.com/">
-                            <IconButton size="lg" fontSize="1.5rem" icon={<FaLinkedinIn />} borderRadius="full" aria-label="Linkedin" backgroundColor="#171717" _hover={{ backgroundColor: '#555' }} />
-                        </Link>
-                    </ListItem>
+                    <MediaLinksItem href="https://www.facebook.com/" icon={<FaFacebookF />} label="Facebook" backgroundColor="#7c2c0c" hover={{ backgroundColor: '#efe4d1', color: '#3b5998' }} />
+                    <MediaLinksItem href="https://twitter.com/" icon={<FaTwitter />} label="Twitter" />
+                    <MediaLinksItem href="https://www.google.com/" icon={<FaGoogle />} label="Google" />
+                    <MediaLinksItem href="https://www.linkedin.com/" icon={<FaLinkedinIn />} label="Linkedin" isLast />
                 </List>
             </ListItem>
 
             <ListItem maxWidth="15rem">
-                <Heading as="h4" pb={8} fontSize="1.5rem" textTransform="uppercase" whiteSpace="nowrap">
-                    Our Repair Center
-                </Heading>
-
+                <SectionHeading>Our Repair Center</SectionHeading>
                 <Text opacity="0.5">Lorem ipsum dolor sit, amet conse ctet ur adipi sicing elit 30Levcamsamoajajahavlovemorethenthatajaj.</Text>
             </ListItem>
         </List>
