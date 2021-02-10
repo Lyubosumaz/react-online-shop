9a0857dd5853e8debca789cfc131b80b1db9949f
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-02-11 01:13:25 +0200

    updeting SelectField

--- web/src/components/form/SelectField/index.tsx
+++ web/src/components/form/SelectField/index.tsx
@@ -1,22 +1,40 @@
-import { FormControl, FormLabel, Select } from '@chakra-ui/react';
+import { Box, FormLabel, Select } from '@chakra-ui/react';
 import React from 'react';
+import { FaCaretDown, FaExclamation } from 'react-icons/fa';
 
-type SelectFieldProps = {
-    callback: (arg: string) => void;
+type OptionType = {
+    value: string;
+    text: string;
 };
 
-const SelectField: React.FC<SelectFieldProps> = ({ callback }) => {
+interface SelectFieldProps {
+    options: OptionType[];
+    label: string;
+    placeholder: string;
+    callback: (arg: string) => void;
+}
+
+const SelectField: React.FC<SelectFieldProps> = ({ options, label, placeholder, callback }) => {
+    const isPopulated = Array.isArray(options) && options.length != 0;
     const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         callback(event.target.value);
     };
 
     return (
-        <FormControl isInvalid={!!false}>
-            <FormLabel htmlFor="category">Category</FormLabel>
-            <Select onChange={handleChange} name="category" placeholder="Select category">
-                <option value="all">All</option>
-            </Select>
-        </FormControl>
+        <Box>
+            <FormLabel>{label}</FormLabel>
+            {isPopulated ? (
+                <Select icon={<FaCaretDown />} onChange={handleChange} placeholder={placeholder}>
+                    {options.map((el, i) => (
+                        <option key={i} value={el.value}>
+                            {el.text}
+                        </option>
+                    ))}
+                </Select>
+            ) : (
+                <Select icon={<FaExclamation />} placeholder={`${label}'s are empty`} isDisabled />
+            )}
+        </Box>
     );
 };
 
--- web/src/pages/create-item/index.tsx
+++ web/src/pages/create-item/index.tsx
@@ -18,34 +18,48 @@ const CreateItem: React.FC<{}> = ({}) => {
         setSelectCategory(category);
     };
 
+    const categoryList = [
+        { value: 'all', text: 'All' },
+        { value: 'gaming', text: 'Gaming' },
+        { value: 'bookstore', text: 'Bookstore' },
+        { value: 'music', text: 'Music' },
+        { value: 'movies', text: 'Movies' },
+        { value: 'merchandise', text: 'Merchandise' },
+        { value: 'laptops', text: 'Laptops' },
+        { value: 'computers', text: 'Computers' },
+        { value: 'audio', text: 'Audio' },
+        { value: 'hi-fi', text: 'Hi-Fi' },
+        { value: 'mobile_devices', text: 'Mobile devices' },
+        { value: 'tv', text: 'TV' },
+        { value: 'monitors', text: 'Monitors' },
+        { value: 'video', text: 'Video' },
+        { value: 'toys', text: 'Toys' },
+        { value: 'mother', text: 'Mother' },
+        { value: 'baby', text: 'Baby' },
+        { value: 'puzzles', text: 'Puzzles' },
+        { value: 'games', text: 'Games' },
+    ];
+
     return (
         <MainLayout variant="small">
             <Formik
                 initialValues={{ title: '', description: '', price: '' }}
                 onSubmit={async (values: any) => {
                     const witchSelectFields = { ...values, category: selectCategory };
-                    console.log(witchSelectFields);
-                    // const { errors } = await createItem({
-                    //     variables: { input: witchSelectFields },
-                    //     update: (cache) => {
-                    //         cache.evict({ fieldName: 'items:{}' });
-                    //     },
-                    // });
-                    // if (!errors) {
-                    //     router.push('/');
-                    // }
+                    const { errors } = await createItem({
+                        variables: { input: witchSelectFields },
+                        update: (cache) => {
+                            cache.evict({ fieldName: 'items:{}' });
+                        },
+                    });
+                    if (!errors) {
+                        router.push('/');
+                    }
                 }}
             >
                 {({ isSubmitting }) => (
                     <Form>
-                        {/* <FormControl isInvalid={!!error}>
-                            <FormLabel htmlFor="category">Category</FormLabel>
-                            <Select value={selectCategory} onChange={handleChange} name="category" placeholder="Select category">
-                                <option value="all">All</option>
-                            </Select>
-                            {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
-                        </FormControl> */}
-                        <SelectField callback={_selectCategory} />
+                        <SelectField options={categoryList} placeholder="Select category" label="Category" callback={_selectCategory} />
                         <InputField name="title" placeholder="title" label="Title" />
                         <InputField name="price" placeholder="price" label="Price" />
                         <InputField name="description" placeholder="item description..." label="Description" isTextarea />
