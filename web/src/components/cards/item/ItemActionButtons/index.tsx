5306923719034f025b2829ea47aefd89603db9e2
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-02-10 12:44:46 +0200

    update ItemActionButtons

--- web/src/components/cards/item/ItemActionButtons/index.tsx
+++ web/src/components/cards/item/ItemActionButtons/index.tsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, Link } from '@chakra-ui/react';
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Box, Button, IconButton, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import React from 'react';
 import { FaEdit, FaTrashAlt } from 'react-icons/fa';
@@ -11,6 +11,9 @@ interface ItemActionButtonsProps {
 export const ItemActionButtons: React.FC<ItemActionButtonsProps> = ({ id, creatorId }) => {
     const { data: meData } = useMeQuery();
     const [deleteItem] = useDeleteItemMutation();
+    const [isOpen, setIsOpen] = React.useState(false);
+    const onClose = () => setIsOpen(false);
+    const cancelRef = React.useRef<HTMLButtonElement>(null);
 
     if (meData?.me?.id !== creatorId) {
         return null;
@@ -22,19 +25,39 @@ export const ItemActionButtons: React.FC<ItemActionButtonsProps> = ({ id, creato
                 <IconButton as={Link} mr={4} icon={<FaEdit />} aria-label="Edit Item" />
             </NextLink>
 
-            <IconButton
-                icon={<FaTrashAlt />}
-                aria-label="Delete Item"
-                onClick={() => {
-                    deleteItem({
-                        variables: { id },
-                        update: (cache) => {
-                            // Item:77
-                            cache.evict({ id: 'Item:' + id });
-                        },
-                    });
-                }}
-            />
+            <IconButton icon={<FaTrashAlt />} aria-label="Delete Item" onClick={() => setIsOpen(true)} />
+            <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                            Delete Item
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>Are you sure? You can't undo this action afterwards.</AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button
+                                ml={3}
+                                colorScheme="red"
+                                onClick={() => {
+                                    deleteItem({
+                                        variables: { id },
+                                        update: (cache) => {
+                                            // Item:77
+                                            cache.evict({ id: 'Item:' + id });
+                                        },
+                                    });
+                                }}
+                            >
+                                Delete
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
         </Box>
     );
 };
