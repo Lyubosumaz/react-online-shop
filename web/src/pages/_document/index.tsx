4b2e9ffacad90e3188909c7944406be6a05fa49f
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-02-22 23:18:01 +0200

    update _document

--- web/src/pages/_document/index.tsx
+++ web/src/pages/_document/index.tsx
@@ -17,6 +17,9 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
+                    <meta charSet="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
                 </Head>
