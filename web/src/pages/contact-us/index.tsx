5dad8a9090563712df418adf1b1eb8c92f3fc92e
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-03-08 23:52:25 +0200

    update contact us

--- web/src/pages/contact-us/index.tsx
+++ web/src/pages/contact-us/index.tsx
@@ -1,12 +1,17 @@
+import ColumnWrapper from '@/layouts/ColumnWrapper';
+import MainLayout from '@/layouts/MainLayout';
+import { withApollo } from '@/utils/withApollo';
 import React from 'react';
 
-const ContactUs: React.FC<{}> = ({}) => {
+const ContactUs: React.FC<{}> = ({ }) => {
     return (
-        <>
-            <h1>ContactUs</h1>
-            <div>ContactUs</div>
-        </>
+        <MainLayout>
+            <ColumnWrapper
+                left={<div>Form</div>}
+                right={<div style={{ width: "540px", height: "370px", display: "flex", justifyContent: "center", alignItems: "center", background: "gray" }}><span>Google Map</span></div>}
+            />
+        </MainLayout>
     );
 };
 
-export default ContactUs;
+export default withApollo({ ssr: true })(ContactUs);
