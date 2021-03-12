13622800c29a5b710fa1bc392ee5d7104e721161
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-03-12 23:40:03 +0200

    update styles in about-us

--- web/src/pages/about-us/index.tsx
+++ web/src/pages/about-us/index.tsx
@@ -1,14 +1,27 @@
 import ColumnWrapper from '@/layouts/ColumnWrapper';
 import MainLayout from '@/layouts/MainLayout';
 import { withApollo } from '@/utils/withApollo';
-import { Box, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Button, Heading, Image, Text, useColorModeValue, useToken } from '@chakra-ui/react';
 import React from 'react';
 
 const AboutBody: React.FC<{}> = ({ }) => {
+    const [lightColor, darkColor] = useToken("colors", ["primaryL.700", "primaryD.500"]);
+    const headerColor = useColorModeValue(lightColor, darkColor);
+
     return (
         <Box>
-            <Heading>About Us</Heading>
-            <Text>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised</Text>
+            <Heading mb="1rem" color={headerColor}>About Us</Heading>
+            <Text mb="1.5rem">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised</Text>
+            <Button
+                p="1.5rem 2rem"
+                border="0.1rem solid"
+                borderColor={headerColor}
+                background="transparent"
+                rounded={0}
+                color={headerColor}
+            >
+                Read More
+            </Button>
         </Box>
     );
 }
