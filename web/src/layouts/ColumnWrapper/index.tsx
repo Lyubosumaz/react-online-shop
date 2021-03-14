c9ab24f7c58633c3d96c9e9ed501dacb45910f42
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-03-14 23:47:05 +0200

    adding BorderedColumn

--- web/src/layouts/ColumnWrapper/index.tsx
+++ web/src/layouts/ColumnWrapper/index.tsx
@@ -6,48 +6,53 @@ interface ColumnProps {
     variant?: ColumnVariant;
 }
 
-const Column: React.FC<ColumnProps> = ({ children, variant = 'regular' }) => {
+const BorderedColumn: React.FC<ColumnProps> = ({ children }) => {
     const [lightColor, darkColor] = useToken("colors", ["primaryL.700", "primaryD.500"]);
     const boColor = useColorModeValue(lightColor, darkColor);
+    const sharedStyles = {
+        width: "6.5rem",
+        height: "6.5rem",
+        content: '""',
+        display: "block",
+        position: "absolute"
+    }
 
+    return (
+        <Box
+            as="section"
+            margin="2.15rem"
+            padding="2rem 3.5rem"
+            position="relative"
+            border="0.1rem solid"
+            borderColor={boColor}
+            _before={{
+                ...sharedStyles,
+                top: "-2.15rem",
+                left: "-2.15rem",
+                borderTop: "0.85rem solid",
+                borderLeft: "0.85rem solid",
+                borderColor: boColor
+            }}
+            _after={{
+                ...sharedStyles,
+                right: "-2.15rem",
+                bottom: "-2.15rem",
+                borderRight: "0.85rem solid",
+                borderBottom: "0.85rem solid",
+                borderColor: boColor
+            }}
+        >
+            {children}
+        </Box>);
+}
+
+const Column: React.FC<ColumnProps> = ({ children, variant = 'regular' }) => {
     return (
         <Flex padding="0 1rem" justifyContent="center" alignItems="center" flexBasis="50%">
             {variant === "regular" ?
-                <>{children}</>
+                <Box as="section">{children}</Box>
                 :
-                <Box
-                    margin="2.15rem"
-                    padding="2rem 3.5rem"
-                    position="relative"
-                    border="0.1rem solid"
-                    borderColor={boColor}
-                    _before={{
-                        width: "6.5rem",
-                        height: "6.5rem",
-                        content: '""',
-                        display: "block",
-                        position: "absolute",
-                        top: "-2.15rem",
-                        left: "-2.15rem",
-                        borderTop: "0.85rem solid",
-                        borderLeft: "0.85rem solid",
-                        borderColor: boColor
-                    }}
-                    _after={{
-                        width: "6.5rem",
-                        height: "6.5rem",
-                        content: '""',
-                        display: "block",
-                        position: "absolute",
-                        right: "-2.15rem",
-                        bottom: "-2.15rem",
-                        borderRight: "0.85rem solid",
-                        borderBottom: "0.85rem solid",
-                        borderColor: boColor
-                    }}
-                >
-                    {children}
-                </Box>
+                <BorderedColumn>{children}</BorderedColumn>
             }
         </Flex>
     )
