6ad8861240716a719ad115fe53470d1079e93eca
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-03-19 19:34:35 +0200

    update contact-us

--- web/src/pages/contact-us/ContactUsForm/index.tsx
+++ web/src/pages/contact-us/ContactUsForm/index.tsx
@@ -26,15 +26,18 @@ const ContactUsForm: React.FC<{}> = ({ }) => {
                     <Form style={{ width: '100%' }}>
                         <Field name="name">
                             {({ field, form }: any) => (
-                                <FormControl mb={6} isInvalid={!!form.errors.name && !!form.touched.name}>
+                                <FormControl
+                                    mb={6}
+                                    isInvalid={!!form.errors.name && !!form.touched.name}
+                                >
                                     <Input
                                         {...field}
-                                        placeholder={form.errors.name ? `Name ${form.errors.name}` : "Name"}
+                                        placeholder={!!form.errors.name && !!form.touched.name ? `Name ${form.errors.name}` : "Name"}
                                         fontSize="md"
                                         p="2rem 1rem"
                                         borderColor={mainColor}
                                         color={mainColor}
-                                        _placeholder={{ color: mainColor }}
+                                        _placeholder={{ color: !!form.errors.name && !!form.touched.name ? "red" : mainColor }}
                                         _hover={{ borderColor: mainColor }}
                                         _focus={{ borderColor: mainColor }}
                                     />
@@ -44,15 +47,18 @@ const ContactUsForm: React.FC<{}> = ({ }) => {
 
                         <Field name="email">
                             {({ field, form }: any) => (
-                                <FormControl mb={6} isInvalid={!!form.errors.email && !!form.touched.email}>
+                                <FormControl
+                                    mb={6}
+                                    isInvalid={!!form.errors.email && !!form.touched.email}
+                                >
                                     <Input
                                         {...field}
-                                        placeholder="Email"
+                                        placeholder={!!form.errors.email && !!form.touched.email ? `Email ${form.errors.email}` : "Email"}
                                         fontSize="md"
                                         p="2rem 1rem"
                                         borderColor={mainColor}
                                         color={mainColor}
-                                        _placeholder={{ color: mainColor }}
+                                        _placeholder={{ color: !!form.errors.email && !!form.touched.email ? "red" : mainColor }}
                                         _hover={{ borderColor: mainColor }}
                                         _focus={{ borderColor: mainColor }}
                                     />
@@ -62,15 +68,18 @@ const ContactUsForm: React.FC<{}> = ({ }) => {
 
                         <Field name="phoneNumber">
                             {({ field, form }: any) => (
-                                <FormControl mb={6} isInvalid={!!form.errors.phoneNumber && !!form.touched.phoneNumber}>
+                                <FormControl
+                                    mb={6}
+                                    isInvalid={!!form.errors.phoneNumber && !!form.touched.phoneNumber}
+                                >
                                     <Input
                                         {...field}
-                                        placeholder="Phone Number"
+                                        placeholder={!!form.errors.phoneNumber && !!form.touched.phoneNumber ? `Phone Number ${form.errors.phoneNumber}` : "Phone Number"}
                                         fontSize="md"
                                         p="2rem 1rem"
                                         borderColor={mainColor}
                                         color={mainColor}
-                                        _placeholder={{ color: mainColor }}
+                                        _placeholder={{ color: !!form.errors.phoneNumber && !!form.touched.phoneNumber ? "red" : mainColor }}
                                         _hover={{ borderColor: mainColor }}
                                         _focus={{ borderColor: mainColor }}
                                     />
@@ -83,13 +92,13 @@ const ContactUsForm: React.FC<{}> = ({ }) => {
                                 <FormControl isInvalid={!!form.errors.massage && !!form.touched.massage}>
                                     <Textarea
                                         {...field}
-                                        placeholder="Massage"
+                                        placeholder={!!form.errors.massage && !!form.touched.massage ? `Massage ${form.errors.massage}` : "Massage"}
                                         fontSize="md"
                                         h="2%"
                                         p="2rem 1rem"
                                         borderColor={mainColor}
                                         color={mainColor}
-                                        _placeholder={{ color: mainColor }}
+                                        _placeholder={{ color: !!form.errors.massage && !!form.touched.massage ? "red" : mainColor }}
                                         _hover={{ borderColor: mainColor }}
                                         _focus={{ borderColor: mainColor }}
                                     />
@@ -97,7 +106,7 @@ const ContactUsForm: React.FC<{}> = ({ }) => {
                             )}
                         </Field>
 
-                        <Box mt="3rem" d="flex" justifyContent="center">
+                        <Box mt="2rem" d="flex" justifyContent="center">
                             <Button
                                 type="submit"
                                 isLoading={isSubmitting}
--- web/src/pages/contact-us/ContactUsMap/index.tsx
+++ web/src/pages/contact-us/ContactUsMap/index.tsx
@@ -6,12 +6,14 @@ const ContactUsMap: React.FC<{}> = ({ }) => {
             style={{
                 width: "540px",
                 height: "370px",
-                marginBottom: "1.5rem",
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
                 background: "gray"
-            }}><span>Google Map</span></div>
+            }}
+        >
+            <span>Google Map</span>
+        </div>
     );
 };
 
