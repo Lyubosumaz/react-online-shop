5bc01ae07b3e49c157827390531a748b9d4d528c
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-03-21 01:07:29 +0200

    adding google apt key

--- web/src/pages/contact-us/ContactUsMap/index.tsx
+++ web/src/pages/contact-us/ContactUsMap/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps";
 
 const ContactUsMap: React.FC<{}> = ({ }) => {
-    const defaultCenter = { lat: 40.748817, lng: -73.985428 };
+    const defaultCenter = { lat: 42.698334, lng: 23.319941 };
     const defaultOptions = { scrollwheel: false };
-
     const RegularMap = withScriptjs(
         withGoogleMap((props: any) => (
             <GoogleMap
-                defaultZoom={8}
+                defaultZoom={10}
                 defaultCenter={defaultCenter}
                 defaultOptions={defaultOptions}
             >
@@ -18,24 +17,13 @@ const ContactUsMap: React.FC<{}> = ({ }) => {
     );
 
     const loadingElementStyle = { height: '100%' };
-    const containerElementStyle = { height: '280px' };
+    const containerElementStyle = { width: "540px", height: '370px' };
     const mapElementStyle = { height: '100%' };
+    const apiKey = 'AIzaSyA_9cyXkdJAawD7utSrbBD31kQ608L0opQ';
 
     return (
-        // <div
-        //     style={{
-        //         width: "540px",
-        //         height: "370px",
-        //         display: "flex",
-        //         justifyContent: "center",
-        //         alignItems: "center",
-        //         background: "gray"
-        //     }}
-        // >
-        //     <span>Google Map</span>
-        // </div>
         <RegularMap
-            googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${apiKey}`}
             loadingElement={<div style={loadingElementStyle} />}
             containerElement={<div style={containerElementStyle} />}
             mapElement={<div style={mapElementStyle} />}
