af0f14b611f233584ddc0f839d592d741dd87812
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-03-22 23:40:27 +0200

    update cart page

--- web/src/pages/cart/index.tsx
+++ web/src/pages/cart/index.tsx
@@ -1,12 +1,45 @@
+import MainLayout from '@/layouts/MainLayout';
 import { withApollo } from '@/utils/withApollo';
+import { Table, Tbody, Td, Tfoot, Th, Thead, Tr } from '@chakra-ui/react';
 import React from 'react';
 
-const Cart: React.FC<{}> = ({}) => {
+const Cart: React.FC<{}> = ({ }) => {
     return (
-        <>
-            <h1>Cart</h1>
-            <div>Cart</div>
-        </>
+        <MainLayout>
+            <Table size="sm" m="4rem 0" border="0.1rem solid" borderColor="gray.400">
+                <Thead>
+                    <Tr>
+                        <Th>To convert</Th>
+                        <Th>into</Th>
+                        <Th isNumeric>multiply by</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    <Tr>
+                        <Td>inches</Td>
+                        <Td>millimetres (mm)</Td>
+                        <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                        <Td>feet</Td>
+                        <Td>centimetres (cm)</Td>
+                        <Td isNumeric>30.48</Td>
+                    </Tr>
+                    <Tr>
+                        <Td>yards</Td>
+                        <Td>metres (m)</Td>
+                        <Td isNumeric>0.91444</Td>
+                    </Tr>
+                </Tbody>
+                <Tfoot>
+                    <Tr>
+                        <Th>To convert</Th>
+                        <Th>into</Th>
+                        <Th isNumeric>multiply by</Th>
+                    </Tr>
+                </Tfoot>
+            </Table>
+        </MainLayout>
     );
 };
 
