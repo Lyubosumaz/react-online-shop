44ffc248fb994403eba5aee2c84dffc48573d585
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-03-26 23:57:36 +0200

    update Quantity component

--- web/src/components/form/Quantity/index.tsx
+++ web/src/components/form/Quantity/index.tsx
@@ -6,9 +6,9 @@ import { FaMinus, FaPlus } from 'react-icons/fa';
 const Quantity: React.FC<{}> = ({ }) => {
     return (
         <Flex>
-            <IconButton aria-label="decrement" icon={<FaMinus />} />
-            <Input />
-            <IconButton aria-label="increment" icon={<FaPlus />} />
+            <IconButton aria-label="decrement" icon={<FaMinus />} rounded={0} />
+            <Input w="3rem" p="0 0.2rem" textAlign="center" rounded={0} />
+            <IconButton aria-label="increment" icon={<FaPlus />} rounded={0} />
         </Flex>
     );
 };
