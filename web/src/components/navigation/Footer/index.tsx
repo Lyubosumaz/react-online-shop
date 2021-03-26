c1b236ab5df88d1041f0c4c0f2e9a5c502add4e8
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-03-26 23:52:30 +0200

    update Footer component

--- web/src/components/navigation/Footer/index.tsx
+++ web/src/components/navigation/Footer/index.tsx
@@ -1,12 +1,21 @@
 import MainWrapper from '@/layouts/MainWrapper';
+import { useColorModeValue, useToken } from '@chakra-ui/react';
 import React from 'react';
 import CopyRights from './CopyRights';
 import ShopDetails from './ShopDetails';
 import UtilityLinks from './UtilityLinks';
 
-const Footer: React.FC<{}> = ({}) => {
+const Footer: React.FC<{}> = ({ }) => {
+    const [lightColor, darkColor] = useToken("colors", ["#100703", "primaryD.900"]);
+    const bgColor = useColorModeValue(lightColor, darkColor);
+
     return (
-        <footer style={{ backgroundColor: '#100703', color: '#fcfcfc' }}>
+        // TODO theme have big palettes without '#100703'
+        <footer style={{
+            // backgroundColor: '#100703',
+            backgroundColor: bgColor,
+            color: '#fcfcfc'
+        }}>
             <MainWrapper>
                 <ShopDetails />
                 <UtilityLinks />
