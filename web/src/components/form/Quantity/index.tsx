a477045b4de12ca9b75f57470afea2f46b5d3be3
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-03-27 23:53:38 +0200

    update Quantity component

--- web/src/components/form/Quantity/index.tsx
+++ web/src/components/form/Quantity/index.tsx
@@ -1,9 +1,10 @@
 import { Flex } from '@chakra-ui/layout';
 import { IconButton, Input } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { FaMinus, FaPlus } from 'react-icons/fa';
 
 const Quantity: React.FC<{}> = ({ }) => {
+    const [valueQuantity, setValueQuantity] = useState(-1)
 
     return (
         <Flex>
@@ -11,20 +12,20 @@ const Quantity: React.FC<{}> = ({ }) => {
                 aria-label="decrement"
                 icon={<FaMinus />}
                 rounded={0}
-                onClick={() => console.log("--")}
+                onClick={() => setValueQuantity((prevState): number => prevState - 1)}
             />
             <Input
                 w="3rem"
                 p="0 0.2rem"
                 textAlign="center"
                 rounded={0}
-                value="-1"
+                value={valueQuantity}
             />
             <IconButton
                 aria-label="increment"
                 icon={<FaPlus />}
                 rounded={0}
-                onClick={() => console.log("++")}
+                onClick={() => setValueQuantity((prevState): number => prevState + 1)}
             />
         </Flex>
     );
