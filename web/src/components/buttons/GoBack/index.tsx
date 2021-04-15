c15f917dcfa3241dfc44ea6f87d38752bdb22df4
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-04-15 23:57:24 +0300

    quick save

--- web/src/components/buttons/GoBack/index.tsx
+++ web/src/components/buttons/GoBack/index.tsx
@@ -1,4 +1,4 @@
-import { IconButton, useColorMode } from '@chakra-ui/react';
+import { IconButton, useColorMode, useColorModeValue, useToken } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { FC, useEffect, useState } from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
@@ -9,8 +9,10 @@ interface GoBackProps {
 
 type ControlStylesType = {
     size?: string;
-    color: string;
-    bgColor: string;
+    color?: string;
+    bgColor?: string;
+    border?: string;
+    borderColor?: string;
     _hover: {
         bgColor: string,
         color?: string
@@ -21,6 +23,8 @@ export const GoBack: FC<GoBackProps> = ({ control = 'default' }) => {
     const router = useRouter();
     const { colorMode } = useColorMode();
     const [controlStyles, setControlStyles] = useState<ControlStylesType>()
+    const [bgLightColor, bgDarkColor] = useToken("colors", ["primaryL.600", "primaryD.500"]);
+    const bgColor = useColorModeValue(bgLightColor, bgDarkColor);
 
     useEffect(() => {
         stylesState(control)
@@ -32,9 +36,9 @@ export const GoBack: FC<GoBackProps> = ({ control = 'default' }) => {
             case "product":
                 styles = {
                     size: "sm",
-                    color: colorMode === 'light' ? 'primaryD.50' : 'primaryD.900',
-                    bgColor: colorMode === 'light' ? 'teal.500' : 'teal.200',
-                    _hover: { bgColor: colorMode === 'light' ? 'teal.600' : 'teal.300' },
+                    border: "0.1rem solid",
+                    borderColor: bgColor,
+                    _hover: { bgColor: bgColor },
                 }
                 break;
             case "custom":
