2b4279487e62ed09c35cb48f57bf75c5547a274a
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-04-16 23:59:31 +0300

    update theme

--- web/src/theme.tsx
+++ web/src/theme.tsx
@@ -1,32 +1,5 @@
+import { Card } from '@/lib/chakra-ui/component-styles/NavBarButton';
 import { extendTheme } from '@chakra-ui/react';
-
-const Card = {
-    // The styles all Cards have in common
-    baseStyle: {
-        display: "flex",
-        flexDirection: "column",
-        background: "white",
-        alignItems: "center",
-        gap: 6,
-    },
-    // Two variants: rounded and smooth
-    variants: {
-        rounded: {
-            padding: 8,
-            borderRadius: "xl",
-            boxShadow: "xl",
-        },
-        smooth: {
-            padding: 6,
-            borderRadius: "base",
-            boxShadow: "md",
-        },
-    },
-    // The default variant value
-    defaultProps: {
-        variant: "smooth",
-    },
-}
 
 const customTheme = {
     initialColorMode: 'light',
