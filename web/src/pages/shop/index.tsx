e916d25955ff39f3cb94abe16b89d5fe1ea412d2
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-04-27 18:14:41 +0300

    quick save

--- web/src/pages/shop/index.tsx
+++ web/src/pages/shop/index.tsx
@@ -2,15 +2,9 @@ import Product from "@/components/cards/Product";
 import { useItemsQuery } from '@/generated/graphql';
 import MainLayout from '@/layouts/MainLayout';
 import { withApollo } from '@/utils/withApollo';
-import { Box, Button, Flex, Link, List, ListItem, Text, useColorModeValue, useStyleConfig, useToken } from '@chakra-ui/react';
+import { Box, Button, Flex, Link, List, ListItem, Text, useColorModeValue, useToken } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import React from "react";
-
-function CustomBadge(props: any) {
-    const { size, variant, ...rest } = props
-    const styles = useStyleConfig("Card", { size, variant })
-    return <Box as="span" sx={styles} {...rest} />
-}
 
 const Shop = () => {
     const { data, error, loading, fetchMore, variables } = useItemsQuery({
@@ -36,8 +30,6 @@ const Shop = () => {
 
     return (
         <MainLayout>
-            <CustomBadge>i'm here</CustomBadge>
-
             {!data && loading
                 ? <Text>loading...</Text>
                 :
@@ -61,50 +53,55 @@ const Shop = () => {
                     {data?.items.items.length
                         ? data!.items.items.map((p) => !p
                             ? null
-                            :
-                            <ListItem
-                                key={p.id}
-                                w={gridWidth}
-                                my={gridHeight}
-                                p={5}
-                                display="flex"
-                                flexDirection="column"
-                                shadow="md"
-                                border="0.1rem solid"
-                                borderColor={color}
-                                _hover={{
-                                    bgColor: "secondaryL.100",
-                                    borderColor: "secondaryL.100",
-                                }}
-                            >
-                                <Product data={p} />
-                            </ListItem>)
-                        :
-                        <ListItem d="flex" alignItems="center">
-                            <Box mr={5}>Nothing was created so far!</Box>
-                            <NextLink href="/create-item">
-                                <Button as={Link} mr={4}>create item</Button>
-                            </NextLink>
-                        </ListItem>
+                            : (
+                                <ListItem
+                                    key={p.id}
+                                    w={gridWidth}
+                                    my={gridHeight}
+                                    p={5}
+                                    display="flex"
+                                    flexDirection="column"
+                                    shadow="md"
+                                    border="0.1rem solid"
+                                    borderColor={color}
+                                    _hover={{
+                                        bgColor: "secondaryL.100",
+                                        borderColor: "secondaryL.100",
+                                    }}
+                                >
+                                    <Product data={p} />
+                                </ListItem>
+                            )
+                        )
+                        : (
+
+                            <ListItem d="flex" alignItems="center">
+                                <Box mr={5}>Nothing was created so far!</Box>
+                                <NextLink href="/create-item">
+                                    <Button as={Link} mr={4}>create item</Button>
+                                </NextLink>
+                            </ListItem>
+                        )
                     }
                 </List>
             }
             {data && data.items.hasMore
-                ?
-                <Flex justify="center">
-                    <Button
-                        onClick={() => {
-                            fetchMore({
-                                variables: {
-                                    limit: variables?.limit,
-                                    cursor: data.items.items[data.items.items.length - 1].createdAt,
-                                },
-                            });
-                        }}
-                        isLoading={loading}
-                        my={8}
-                    >load more</Button>
-                </Flex>
+                ? (
+                    <Flex justify="center">
+                        <Button
+                            onClick={() => {
+                                fetchMore({
+                                    variables: {
+                                        limit: variables?.limit,
+                                        cursor: data.items.items[data.items.items.length - 1].createdAt,
+                                    },
+                                });
+                            }}
+                            isLoading={loading}
+                            my={8}
+                        >load more</Button>
+                    </Flex>
+                )
                 : null
             }
         </MainLayout>
